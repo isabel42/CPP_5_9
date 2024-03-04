00329962a8a87cba14d459973906c181ad79dab7
isabel42 <106957731+isabel42@users.noreply.github.com>
2024-03-04 12:38:31 +0100

    main.cpp change for better test ex01

--- CPP_5/ex01/main.cpp
+++ CPP_5/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 14:18:06 by itovar-n          #+#    #+#             */
-/*   Updated: 2024/02/22 15:42:02 by itovar-n         ###   ########.fr       */
+/*   Updated: 2024/03/04 11:42:03 by itovar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,26 @@ int main()
 {
 	Bureaucrat *b1 = new Bureaucrat();
 	Bureaucrat *b2 = new Bureaucrat(50, "isa");
-	Form *f1 = new Form();
-	Form *f2 = new Form ("Andres", 49, 5);
-	// try
-	// {
-	// 	Bureaucrat b3(249,"Bill");
-	// }
-	// catch (Bureaucrat::Exception &e)
-	// {
-	// 	std::cout << e.what();
-	// }
-
-	// try
-	// {
-	// 	Form f3("Miguel", 249, 5);
-	// }
-	// catch (Form::Exception_parent &e)
-	// {
-	// 	std::cout << e.what();
-	// }
-
-	// std::cout << *b1 << std::endl;
-	// std::cout << *b2 << std::endl;
+	
 	std::cout << "------" << std::endl;
+	Form *f1 = new Form();
 	std::cout << *f1 << std::endl;
 	f1->beSigned(*b2);
 	std::cout << *f1 << std::endl;
-	b2->signForm(*f2);
-	std::cout << "------" << std::endl;
 	
+	std::cout << "------" << std::endl;
+	Form *f2;
+	try
+	{
+		f2 = new Form ("Andres", 449, 5);
+		b2->signForm(*f2);
+	}
+	catch (Form::Exception_parent &e)
+	{
+		std::cout << e.what();
+	}
+	
+	std::cout << "------" << std::endl;
 	delete b1;
 	delete b2;
 	delete f1;
