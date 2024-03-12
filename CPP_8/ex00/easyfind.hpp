05e6000bdeed9eac15c45e7b5ec8f3b6c288399f
isabel42 <106957731+isabel42@users.noreply.github.com>
2024-03-12 16:40:42 +0100

    cpp08

---
+++ CPP_8/ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 15:58:47 by itovar-n          #+#    #+#             */
+/*   Updated: 2024/03/12 16:36:49 by itovar-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+
+template<typename T>
+int easyfind(T &container, int n);
+
+#include "easyfind.tpp"
+
+#endif
